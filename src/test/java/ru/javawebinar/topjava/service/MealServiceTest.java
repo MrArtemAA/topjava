c8acdfb9aa5f61f756e1b241fda7ab708fc1740c
meartemaa <artemaa@gmail.com>
2017-04-19 03:49:15 +0300

    Review corrections

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -23,7 +23,6 @@ import java.time.Month;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static ru.javawebinar.topjava.MealTestData.*;
