721ed2de78d6f6bbbf2bfe39e5533a30f0d7ad4e
meartemaa <artemaa@gmail.com>
2017-05-06 16:46:31 +0300

    1.2 done

--- src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Assume;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.javawebinar.topjava.model.Meal;
@@ -78,8 +79,11 @@ public abstract class AbstractMealServiceTest extends AbstractServiceTest {
                         LocalDate.of(2015, Month.MAY, 30), USER_ID));
     }
 
+
+
     @Test
     public void testValidation() throws Exception {
+        Assume.assumeTrue(!isJdbcProfile());
         validateRootCause(() -> service.save(new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "  ", 300), USER_ID), ConstraintViolationException.class);
         validateRootCause(() -> service.save(new Meal(null, null, "Description", 300), USER_ID), ConstraintViolationException.class);
         validateRootCause(() -> service.save(new Meal(null, of(2015, Month.JUNE, 1, 18, 0), "Description", 9), USER_ID), ConstraintViolationException.class);
