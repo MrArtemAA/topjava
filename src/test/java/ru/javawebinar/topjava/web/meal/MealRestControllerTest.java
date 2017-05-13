11b8da9c4d5d77c0c0d2fb3da58df79da5ba4b31
meartemaa <artemaa@gmail.com>
2017-05-14 00:58:11 +0300

    1 + 2 (partially)

---
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -0,0 +1,81 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.web.AbstractControllerTest;
+import ru.javawebinar.topjava.web.json.JsonUtil;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.javawebinar.topjava.MealTestData.*;
+
+/**
+ * MrArtemAA
+ * 14.05.2017
+ */
+public class MealRestControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = MealRestController.REST_URL + "/";
+
+    @Test
+    public void testGet() throws Exception {
+        mockMvc.perform(get(REST_URL + MEAL1_ID))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(MATCHER.contentMatcher(MEAL1));
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        Meal expected = new Meal(null,
+                LocalDateTime.of(2017, 05, 14, 15, 00,00),
+                "Обед", 1300);
+
+        ResultActions action = mockMvc.perform(post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(expected)))
+        .andExpect(status().isCreated());
+
+        Meal returned = MATCHER.fromJsonAction(action);
+        expected.setId(returned.getId());
+
+        MATCHER.assertEquals(expected, returned);
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        Meal updated = new Meal(MEAL1);
+
+        updated.setDescription("Полдник");
+        updated.setCalories(200);
+        updated.setDateTime(LocalDateTime.of(2017, 05, 14, 16, 30, 00));
+
+        mockMvc.perform(put(REST_URL + MEAL1_ID)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonUtil.writeValue(updated)))
+                .andExpect(status().isOk());
+
+        MATCHER.assertEquals(updated, mealService.get(MEAL1_ID, AuthorizedUser.id()));
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+    }
+
+    @Test
+    public void testGetBetween() throws Exception {
+    }
+
+}
