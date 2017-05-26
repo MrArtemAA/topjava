7644ccc0cf153d8a1393b7c7e5425ffaad95d338
meartemaa <artemaa@gmail.com>
2017-05-27 02:57:35 +0300

    Completed all tasks

--- src/main/webapp/resources/js/mealDatatables.js
+++ src/main/webapp/resources/js/mealDatatables.js
@@ -17,11 +17,21 @@ function clearFilter() {
 
 $(function () {
     datatableApi = $("#datatable").DataTable({
+        "ajax" : {
+            "url" : ajaxUrl,
+            "dataSrc" : ""
+        },
         "paging": false,
         "info": true,
         "columns": [
             {
-                "data": "dateTime"
+                "data": "dateTime",
+                "render": function (date, type, row) {
+                    if (type === 'display') {
+                        return '<span>' + moment(date).format('DD.MM.YYYY HH:mm') + '</span>';
+                    }
+                    return date;
+                }
             },
             {
                 "data": "description"
@@ -30,12 +40,14 @@ $(function () {
                 "data": "calories"
             },
             {
-                "defaultContent": "Edit",
-                "orderable": false
+                "orderable": false,
+                "defaultContent": "",
+                "render": renderEditBtn
             },
             {
-                "defaultContent": "Delete",
-                "orderable": false
+                "orderable": false,
+                "defaultContent": "",
+                "render": renderDeleteBtn
             }
         ],
         "order": [
@@ -43,7 +55,27 @@ $(function () {
                 0,
                 "desc"
             ]
-        ]
+        ],
+        "createdRow": function (row, data, dataIndex) {
+            if (data.exceed) {
+                $(row).addClass("exceeded");
+            } else {
+                $(row).addClass("normal");
+            }
+        },
+        "initComplete": makeEditable
     });
-    makeEditable();
+
+    $('#startDatePicker').datetimepicker({
+        format: 'DD.MM.YYYY'
+    });
+    $('#endDatePicker').datetimepicker({
+        format: 'DD.MM.YYYY'
+    });
+    $('#startTimePicker').datetimepicker({
+        format: 'HH:mm'
+    });
+    $('#endTimePicker').datetimepicker({
+        format: 'HH:mm'
+    });
 });
